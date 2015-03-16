d3e3e22fe7505f42aaad2ae903071a5bcc02f5fe
Lubomir Kosaristan <kosoburak@gmail.com>
2015-03-16 18:28:29 +0100

    Fixed small bugs

--- lib/settings.rb
+++ lib/settings.rb
@@ -3,6 +3,6 @@ require 'settingslogic'
 class Settings < Settingslogic
   CONFIGURATION = 'conf.yml'
 
-  source "#{File.dirname(__FILE__)}/#{CONFIGURATION}"
+  source "#{File.dirname(__FILE__)}/../config/#{CONFIGURATION}"
 
 end
