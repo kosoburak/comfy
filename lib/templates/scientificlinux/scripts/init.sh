ef66129745faffb11107d77dd899d225f9fde58a
Lubomir Kosaristan <kosoburak@gmail.com>
2015-07-01 14:47:26 +0200

    Added Debian 8 support, configured NTPd, added fail2ban and pakiti2client, corrected README file

--- lib/templates/scientificlinux/scripts/init.sh
+++ lib/templates/scientificlinux/scripts/init.sh
@@ -6,15 +6,20 @@ yum -y install http://ftp.astral.ro/mirrors/fedora/pub/epel/7/x86_64/e/epel-rele
 yum -y update
 # install new packages
 yum -y install cloud-init
+yum -y install fail2ban ntp
 yum -y install qemu-guest-agent
 yum -y install krb5-libs krb5-workstation pam_krb5
 yum -y install vim git
+yum -y remove chrony
 
 # set cloud-init to start after boot
 systemctl enable cloud-init-local
 systemctl enable cloud-init
 systemctl enable cloud-config
 systemctl enable cloud-final
+
+# NTPd start after boot
+systemctl enable ntpd.service
 
 # move configuration file to their right place
 mv /root/cloud.cfg /etc/cloud/cloud.cfg
@@ -23,9 +28,17 @@ mv /root/sshd_config /etc/ssh/sshd_config
 mv /root/10-ipv6.conf /etc/sysctl.d/10-ipv6.conf
 mv /root/grub /etc/default/grub
 mv /root/getty\@ttyS0.service /etc/systemd/system/getty\@ttyS0.service
-
 grub2-mkconfig -o /boot/grub2/grub.cfg
 ln -s /etc/systemd/system/getty\@ttyS0.service /etc/systemd/system/getty.target.wants/getty@ttyS0.service
+mv /root/ntp.conf /etc/ntp.conf
+
+# fail2ban
+mv /root/iptables-multiport.local /etc/fail2ban/action.d/iptables-multiport.local
+mv /root/jail.local /etc/fail2ban/jail.local
+mv /root/fail2ban.local /ect/fail2ban/fail2ban.local
+
+# pakiti-2-client
+rpm -i pakiti-2.1.5-1.noarch.rpm
 
 # remove hardware address (MAC) and UUID from NIC configuration files
 sed -i '/^HWADDR/d' /etc/sysconfig/network-scripts/ifcfg-eth*
