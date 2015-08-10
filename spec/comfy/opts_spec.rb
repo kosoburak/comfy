72b4b00aecbc1d8fa7ce1a94c4b80a35828002a1
Lubomir Kosaristan <kosoburak@gmail.com>
2015-08-10 10:53:08 +0200

    Added a few tests, small refactoring of opts.rb

--- spec/comfy/opts_spec.rb
+++ spec/comfy/opts_spec.rb
@@ -4,8 +4,9 @@ describe Comfy::Opts do
   
   let(:options) {OpenStruct.new}
 
-#  describe '#parse' do
-#  end
+  describe '#parse' do
+
+  end
 
 #  describe '#list_distributions' do
 #  end
@@ -28,14 +29,50 @@ describe Comfy::Opts do
       end
     end
 
-#    context 'with format set' do
-#    end
+    context 'with format set' do
+      before :example do
+        options.formats = [:raw]
+      end
 
-#    context 'with size set' do
-#    end
+      it 'takes format from options' do
+        Comfy::Opts.set_defaults(options)
+        expect(options.formats). to eq([:raw])
+        expect(options.size). to eq(Comfy::Opts::SIZE_DEFAULT)
+        expect(options.debug). to eq(Comfy::Opts::DEBUG_DEFAULT)
+        expect(options.template_dir). to eq(Comfy::Opts::DIR)
+        expect(options.version). to eq(:newest)
+      end
+    end
 
-#    context 'with debug mode on' do
-#    end
+    context 'with size set' do
+      before :example do
+        options.size = 300
+      end
+
+      it 'takes size from options' do
+        Comfy::Opts.set_defaults(options)
+         expect(options.formats). to eq([Comfy::Opts::FORMAT_DEFAULT])
+         expect(options.size). to eq(300)
+         expect(options.debug). to eq(Comfy::Opts::DEBUG_DEFAULT)
+         expect(options.template_dir). to eq(Comfy::Opts::DIR)
+         expect(options.version). to eq(:newest)
+      end
+    end
+
+    context 'with debug mode on' do
+      before :example do
+        options.debug = true
+      end
+
+      it 'debug mode is enabled' do
+        Comfy::Opts.set_defaults(options)
+        expect(options.formats). to eq([Comfy::Opts::FORMAT_DEFAULT])
+        expect(options.size). to eq(Comfy::Opts::SIZE_DEFAULT)
+        expect(options.debug). to be true
+        expect(options.template_dir). to eq(Comfy::Opts::DIR)
+        expect(options.version). to eq(:newest)
+      end
+    end
 
 #    context 'with template dir set' do
 #    end
@@ -48,10 +85,49 @@ describe Comfy::Opts do
 #  describe '#check_files' do
 #  end
 
-#  describe '#check_options_restrictions' do
-#  end
+  describe '#check_options_restrictions' do
+    context 'with disk size < 0' do
+      before :example do
+        options.size = -100
+      end
 
-#  describe '#check_settings_restrictions' do
-#  end
+      it 'Fails with ArgumentError' do
+        expect {Comfy::Opts.check_options_restrictions(options)}.to raise_error(ArgumentError)
+      end
+    end
+
+    context 'with disk size = 0' do
+      before :example do
+        options.size = 0
+      end
+
+      it 'Fails with ArgumentError' do
+        expect {Comfy::Opts.check_options_restrictions(options)}.to raise_error(ArgumentError)
+      end
+    end
+  end
+
+  describe '#check_settings_restrictions' do
+    context 'with missing output directory' do
+      before :example do
+        Settings['output_dir'] = ''
+      end
+
+      it 'Fails with ArgumentError' do
+        expect {Comfy::Opts.check_settings_restrictions(options)}.to raise_error(ArgumentError)
+      end
+    end
+
+    context 'with missing packer cache directory' do
+      before :example do
+        Settings['packer_cache_dir'] = ''
+      end
+
+      it 'Fails with ArgumentError' do
+        expect {Comfy::Opts.check_settings_restrictions(options)}.to raise_error(ArgumentError)
+      end
+    end
+
+  end
 
 end
